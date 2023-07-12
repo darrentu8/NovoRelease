48f0d313e1c458c08ba6db583419c5a678b4f228
darren <darren.tu@deltaww.com>
2023-07-12 17:29:39 +0800

    add image

---
+++ src/components/ann/AnnComponent.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-card class="" style="min-width: 800px;">
+    <div class="bg-grey-3 q-py-lg head flex justify-between items-center">
+      <div class="flex items-center">
+        <img class="q-mr-sm" src="~assets/img/icon/org-g.svg" alt="">
+        <span class="text-h6">System Announcement</span>
+      </div>
+      <q-btn flat round color="primary" @click="showTab = 'Service'" icon="add" label="" />
+    </div>
+    <q-table
+      able-style="overflow-y:auto;overflow-x:hidden;top: -1px;position: relative;background: linear-gradient(rgb(242, 242, 242), transparent) center top / 100% 100px no-repeat local, radial-gradient(at 50% -15px, rgba(0, 0, 0, 0.8), transparent 70%) center top / 100000% 12px scroll;background-repeat: no-repeat;background-attachment: local, scroll;"
+      table-header-style="color:#888888;fontWeight:bold;" flat class="full-width q-table-height" :rows="rows"
+      :columns="columns" row-key="name">
+      <!-- Image -->
+      <template v-slot:body-cell-image="props">
+        <q-td :props="props">
+          <q-img :src="props.row.image" :alt="props.row.image" style="height:30px;" />
+        </q-td>
+      </template>
+      <!-- Actions -->
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn color="primary" round flat @click.stop>
+            <img src="~assets/img/icon/edit.svg" alt="">
+            <q-tooltip>
+              Edit
+            </q-tooltip>
+          </q-btn>
+          <q-btn color="negative" round flat @click.stop>
+            <img src="~assets/img/icon/delete.svg" alt="">
+            <q-tooltip>
+              Delete
+            </q-tooltip>
+          </q-btn>
+        </q-td>
+      </template>
+    </q-table>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import inputRules from 'src/mixins/inputRules.js'
+
+export default defineComponent({
+  name: 'AnnComponent',
+  mixins: [inputRules],
+  props: {
+  },
+  data() {
+    return {
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Name',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'image', align: 'center', label: 'Image', field: 'image', sortable: false },
+        { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
+        { name: 'url', align: 'center', label: 'URL', field: 'url', sortable: true },
+        { name: 'actions', label: '', field: 'actions', sortable: false }
+      ],
+      rows: [
+        {
+          name: 'Remote Manager Cloud',
+          image: 'https://cdn.quasar.dev/app-icons/icon-128x128.png',
+          status: 'active',
+          url: 'https://remotemanager.novohyspace.com/'
+        }
+      ],
+      loading: false,
+      dense: false
+    }
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+})
+</script>
+<style lang="sass" scoped>
+</style>
---
+++ src/components/service/CreateService.vue
@@ -0,0 +1,117 @@
+<template>
+  <q-dialog ref="dialog">
+    <q-card style="width: 100%; max-width: 400px;">
+      <q-card-section class="">
+        <div class="text-h6 text-bold">Create New Service</div>
+      </q-card-section>
+
+      <q-card-section class="q-mx-lg q-my-md">
+        <q-form ref="Form" class="q-gutter-md" @submit.stop="createProject">
+          <q-input filled class="q-mt-xs" type="text" v-model="data.name" label="Name" lazy-rules :rules="[
+            (val) =>
+              (val !== null && val !== '') || 'Please enter a service name']">
+          </q-input>
+          <q-file v-model="image" class="q-mt-xs" label="Upload Image" filled @update:model-value="handleUpload()" :rules="[
+            (val) =>
+              (val !== null && val !== '') || 'Please select a image']">
+            <template v-slot:prepend>
+              <q-icon name="cloud_upload" />
+            </template>
+          </q-file>
+          <div v-if="imageUrl" class="q-mb-lg">
+            <q-img :src="imageUrl" spinner-color="white" style="max-height: 100px; max-width: 320px;"></q-img>
+          </div>
+          <q-input filled class="q-mt-xs" type="text" v-model="data.url" label="URL" lazy-rules :rules="[
+            (val) =>
+              (val !== null && val !== '') || 'Please enter a service URL']">
+          </q-input>
+          <q-card-actions class="q-mt-lg q-pa-none" align="right">
+            <q-btn flat class="text-center text-bold text-primary" @click="hide">Cancel</q-btn>
+            <q-btn unelevated class="q-mb-xs q-px-lg" label="Create" type="submit" color="primary" />
+          </q-card-actions>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import inputRules from 'src/mixins/inputRules.js'
+
+export default defineComponent({
+  name: 'CreatProjectDialog',
+  mixins: [inputRules],
+  data() {
+    return {
+      dense: true,
+      data: {
+        name: '',
+        user_id: '',
+        approve: '1',
+        uid: ''
+      },
+      image: null,
+      imageUrl: ''
+    }
+  },
+  computed: {
+    userData() {
+      return this.$store.state.auth.userData
+    }
+  },
+  methods: {
+    handleUpload() {
+      console.log('handleUpload is triggered')
+      if (this.image) {
+        this.imageUrl = URL.createObjectURL(this.image)
+      }
+    },
+    reset() {
+      this.data = {
+        name: '',
+        image: '',
+        url: ''
+      }
+    },
+    createProject() {
+      this.$refs.Form.validate().then(success => {
+        // console.log('this.userData', this.userData)
+        if (success) {
+          const Data = {
+            name: this.data.name,
+            image: this.data.image,
+            url: this.data.url
+          }
+          this.$store.dispatch('project/createProject', Data)
+            .then(() => {
+              this.$refs.dialog.hide()
+              this.reset()
+            })
+            .then(() => {
+              this.$store.commit('project/setLoading', true)
+              setTimeout(() => {
+                this.$store.dispatch('project/getProject')
+              }, 2000)
+            })
+        } else {
+          this.$q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Need filled'
+          })
+        }
+      })
+    },
+    show() {
+      this.$refs.dialog.show()
+    },
+    hide() {
+      this.$refs.dialog.hide()
+    }
+  }
+})
+</script>
+<style lang="sass" scoped>
+</style>
